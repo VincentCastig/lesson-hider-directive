94a49c7240c98ba59ba5dccf2373c079b1d94bf6
Vincent Castigliola <vincentcastigliola@Vincents-MacBook-Air.local>
2017-07-26 09:04:57 -0500

    direct1

--- js/lessonHider.js
+++ js/lessonHider.js
@@ -11,12 +11,12 @@ angular.module('directivePractice').directive('lessonHider', function(){
         $scope.getSchedule = lessonService.getSchedule();
       },
       link: function(scope, element, attrs){
+          
         scope.getSchedule.then(function(response){
           scope.schedule = response.data
 
           scope.schedule.forEach(function(scheduleDay, index){
-            console.log(attrs)
-            console.log(element)
+
             if(scheduleDay.lesson === scope.ourLesson){
               element.css('text-decoration', 'line-through');
               scope.lessonDay = scheduleDay.weekday
